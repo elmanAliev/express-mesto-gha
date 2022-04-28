7c32101fa109898221a45f4466f3eaca03697ccb
Elman Aliev <elman3605@yandex.ru>
2022-04-28 10:21:56 +0300

    fix: исправить валидацию

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
+const {
+  BadDataError,
+} = require('../errors');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new BadDataError('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,9 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    return next(new BadDataError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
--- models/user.js
+++ models/user.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: false,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Неверный формат ссылки',
+    },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
