9dfa5120d12b8aeb7f7a8d63446fe7dfa0cbf134
Elman Aliev <elman3605@yandex.ru>
2022-05-13 11:31:20 +0300

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const {
 } = require('./controllers/user');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 // импорт роутеров
 const userRouter = require('./routes/user');
 const cardRouter = require('./routes/card');
@@ -28,7 +28,31 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 // подключаем логгер запросов
 app.use(requestLogger);
 
-app.use(cors);
+// app.use(cors);
+const allowedCors = [
+  'http://localhost:3000',
+  'http://elman3605.students.nomoredomains.work',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,26 +1,26 @@
-const allowedCors = [
-  'http://localhost:3000',
-  'http://elman3605.students.nomoredomains.work',
-];
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'http://elman3605.students.nomoredomains.work',
+// ];
 
-module.exports = (req, res, next) => {
-  const { method } = req;
-  const { origin } = req.headers;
+// module.exports = (req, res, next) => {
+//   const { method } = req;
+//   const { origin } = req.headers;
 
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Credentials', true);
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-  return null;
-};
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   next();
+//   return null;
+// };
